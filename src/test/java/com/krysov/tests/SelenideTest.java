5b837d57b3eeb3e96e68851515cccf4ec730f678
Alex Krysov <krysov-95@mail.ru>
2022-12-17 22:53:07 +0600

    added tests

---
+++ src/test/java/com/krysov/tests/SelenideTest.java
@@ -0,0 +1,32 @@
+package com.krysov.tests;
+
+import com.codeborne.selenide.selector.ByText;
+import io.qameta.allure.Owner;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
+import static org.openqa.selenium.By.linkText;
+
+public class SelenideTest extends TestBase{
+
+    @Test
+    @Owner("Alex Krysov")
+    void SelenideSearchTest(){
+        step("Open github.com", () ->
+        open("https://github.com"));
+        step("Open Selenide project", () -> {
+        $(".header-search-input").setValue("selenide").pressEnter();
+        $(linkText("selenide/selenide")).click();
+        });
+        step("Click on Wiki", () ->
+        $("#wiki-tab").click());
+        step("Pages has Soft assertions", () ->
+        $("#wiki-content").$(new ByText("Soft assertions")).shouldHave(text("Soft assertions")));
+        step("Open Soft assertions", () ->
+        $("#wiki-content").$(new ByText("Soft assertions")).click());
+        step("Check JUnit5 example", () ->
+        $(".markdown-body").shouldHave(text("Using JUnit5 extend test class")));
+    }
+}
